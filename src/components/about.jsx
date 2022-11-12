c2c6a508e8e81811e4fd10bb916f04284f112057
Akshay Pawar <99678960+akshaypawar22@users.noreply.github.com>
2022-11-12 18:55:38 +0530

    First Commit

---
+++ src/components/about.jsx
@@ -0,0 +1,133 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <section className="colorlib-about" data-section="about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                                    <div className="col-md-12">
+                                        <div className="about-desc">
+                                            <span className="heading-meta">About Me</span>
+                                            <h2 className="colorlib-heading">Who Am I?</h2>
+                                            <p>I am a MCA student from Sardar Patel Institute of Technology with 8.43 CGPA. I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
+                                            <p>I come to the industry with my skills in programming and wish to learn how to utilize these alongside my soft skills. I define myself as a Commited, hardworking and problem solving personality. I am a team player and very optimistic in tough times. I believe learning never stops and I aim to only grow and upskill myself in the future. I spend at least 2 Hrs a day on my personal learning.
+                                                My aim is to be a part of an organization where I can enhance my knowledge and training towards organizational growth and I am willing to learn in making the organization successful.
+                                                I was very fortunate to be part of SPIT and do some IT projects.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="colorlib-about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                                <span className="heading-meta">What I do?</span>
+                                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                            </div>
+                        </div>
+                        <div className="row row-pt-md">
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-1">
+                                    <span className="icon">
+                                        <i className="icon-bulb" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Web Development </h3>
+                                        <p>I have experience building websites using JavaScript,HTML,CSS and for Backend I know NodeJs</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-3">
+                                    <span className="icon">
+                                        <i className="icon-phone3" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Data Structures & Algorithms</h3>
+                                        <p>As coming from the MCA background, I have good grasp over fundamental concepts of DSA and OOPS and I like to Code in JAVA</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-5">
+                                    <span className="icon">
+                                        <i className="icon-data" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Dev Ops</h3>
+                                        <p>I am Looking forward to work more in DevOps, As of Now I know how to use Git and GitHub and I just started learning AWS, I also have hand on Experiance on RPA Tools like Automation Anywhere.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-2">
+                                    <span className="icon">
+                                        <i className="icon-data" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>DBMS</h3>
+                                        <p>I have knowledge of both SQL and NOSQL databases. In SQL i know MYSQL, Oracle and Mongodb In NOSQL.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-4">
+                                    <span className="icon">
+                                        <i className="icon-data" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Framework</h3>
+                                        <p>I know frameworks like ReactJs, AngularJs and CSR Frameworks like NextJs, in Mobile Programming I am Familiar with Ionic</p>
+                                    </div>
+                                </div>
+                            </div>
+                            {/*
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>Jenkins , Kubernetes , Docker </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div>
+            */}
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>NeedForFeed.com (Summer Term Project) <span>2021-present</span></h2>
+                        <p>This Project aims to reduce the wastage of food by providing Both Selling and Donating Homemade food options also with this website, users can purchase freshly cooked Homemade food from the website.
+                          “NeedForFood.com” allows users to donate extra, fresh food that otherwise will be wasted. Not only extra, users can donate freshly cooked food to poor and needy people also, the main goal of the website is to help Deliver your delicious food to needy one.
+                          Apart from donation users will be able to generate income by selling their cooked HomeMade food through this website.
+                          COD or Online payment both ways users will be able to execute their transactions.
+                          This website helps users to operate like a restaurant in that users only have to take care about preparation of food, all other delivery features will be handled by the website.
+                          Users can buy food according to their choice, through this website i intended to encourage people to do charity.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Web Development Intern <span>Jul 2022- Aug 2022</span></h2>
+                        <p>I done one month internship in Sparks Foundation from month july to august, in internship they given us list of task to complete, where we have to create website and deploy that website in github, i created charity website where users able to donate money with through online payment and after successful transaction a email will be send to user where all details about transaction and users will be written with pdf file.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation at B.K Birla College <span> 2017-2020</span></h2>
+                        <p>I am completed my under-graduation studies in B.K Birla College in 2020. I have taken courses like Physics, Mathmatics and Chemistry and over the years and have better understanding of these subjects. I have also been part of Astronomy Club scince my first year of college. In 2020 I completed My Bsc Physics Degree with 9.32 CGPA from Mumbai University</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2003-2017</span></h2>
+                        <p>I have completed my higher secondary education (HSE) with major subjects as Physics,Chemistry, Maths & Biology with 60.31%. And 79.42% in my SSC, During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
